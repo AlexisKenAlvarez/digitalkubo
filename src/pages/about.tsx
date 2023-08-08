2d453dd7ee1139b9134e5b6660403017c282a274
Rod Cotines <rodcotines@gmail.com>
2023-08-08 12:02:45 +0800

    added subscribe component to about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,17 @@
 import About from "@/views/about/About";
+import Subscribe from "@/views/about/Subscribe";
 import Team from "@/views/about/Team";
 import What from "@/views/about/What";
 
 const about = () => {
-    return (
-        <div className="w-full min-h-screen h-auto">
-            <About />
-            <What />
-            <Team />
-        </div>
-    );
-}
+  return (
+    <div className="w-full min-h-screen h-auto">
+      <About />
+      <What />
+      <Team />
+      <Subscribe />
+    </div>
+  );
+};
 
 export default about;
--- src/views/about/Subscribe.tsx
+++ src/views/about/Subscribe.tsx
@@ -3,21 +3,24 @@ import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-const About = () => {
+const Subscribe = () => {
   return (
     <main className={` ${montserrat.className}`}>
       <div>
         <div className="max-w-full md:w-full h-[356px] bg-[#B4B3AE] flex flex-col items-center justify-center ">
           <h1 className="text-[#D1770E] md:text-[80px] font-bold md:text-center text-[40px] sm:text-[60px]">
             {" "}
-            ABOUT US
+            Want to see more?
           </h1>
-
+          <h1 className="text-[#D1770E] md:text-[60px] font-bold md:text-center text-[30px] sm:text-[60px]">
+            {" "}
+            SUBSCRIBE!
+          </h1>
           <button
             type="button"
             className="text-white bg-[#D1770E] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-[#D1770E] dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
-            See More
+            SUBSCRIBE
           </button>
         </div>
       </div>
@@ -25,4 +28,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default Subscribe;
