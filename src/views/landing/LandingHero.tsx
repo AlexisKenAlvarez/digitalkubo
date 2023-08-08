de33b5de17493ddf1a36f17adc6a739ad9b98a85
iamnards <nardskie19@gmail.com>
2023-08-08 16:22:19 +0800

    Landing page added

---
+++ src/views/landing/LandingHero.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+
+const LandingHero = () => {
+    return (
+        <section className="w-full min-h-screen px-5 py-10 pt-[8rem] relative">
+            <div className="max-w-[1400px] w-full mx-auto">
+                <h1 className="text-[#D1770E] font-primary md:text-[100px] font-bold md:text-left text-[40px] sm:text-[60px]">
+                    {" "}
+                    DIGITALKUBO
+                </h1>
+
+                <p className="text-[#0000004f] font-secondary font-medium md:text-left md:text-2xl max-w-[44rem] my-10 mt-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sed debitis praesentium laudantium architecto repudiandae minus at ipsum quis iusto vel iste, non accusamus magnam ut voluptas? Illo, nobis tempore?
+                </p>
+
+                <Button className=" bg-button hover:bg-nav text-lg mr-2 mb-2 px-10 py-7">
+                    <p className="">See More</p>
+                </Button>
+            </div>
+        </section>
+    );
+}
+
+export default LandingHero;
---
+++ src/views/landing/Latest.tsx
@@ -0,0 +1,14 @@
+const Latest = () => {
+    return (
+        <section className="w-full min-h-screen px-5 py-10 pt-[8rem] relative bg-white">
+            <div className="max-w-[1400px] w-full mx-auto">
+            <h1 className="text-[#D1770E] font-primary md:text-[24x] font-bold md:text-left text-[40px] sm:text-[60px]">
+                    {" "}
+                    LATEST
+                </h1>
+            </div>
+        </section>
+    );
+}
+
+export default Latest;
