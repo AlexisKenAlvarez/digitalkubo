c6d6fccdbede4d99dd82d912ba57804f790da96e
Rod Cotines <rodcotines@gmail.com>
2023-08-08 13:55:41 +0800

    change the color of the button in about and hover

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Montserrat } from "next/font/google";
+import { Button } from "@/components/ui/button";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -12,12 +13,9 @@ const About = () => {
           ABOUT US
         </h1>
 
-        <button
-          type="button"
-          className="text-white bg-[#D1770E] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-[#D1770E] dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
-          See More
-        </button>
+        <Button className=" bg-button hover:bg-nav text-sm px-5 py-2.5 mr-2 mb-2">
+          <p className="">See More</p>
+        </Button>
       </div>
     </main>
   );
--- src/views/about/Subscribe.tsx
+++ src/views/about/Subscribe.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Montserrat } from "next/font/google";
+import { Button } from "@/components/ui/button";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -17,12 +18,9 @@ const Subscribe = () => {
             SUBSCRIBE!
           </h1>
           <div className="py-5">
-            <button
-              type="button"
-              className="text-white bg-[#D1770E] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-[#D1770E] dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            >
-              SUBSCRIBE
-            </button>
+            <Button className=" bg-button hover:bg-nav text-sm px-5 py-2.5 mr-2 mb-2">
+              <p className="">SUBSCRIBE</p>
+            </Button>
           </div>
         </div>
       </div>
