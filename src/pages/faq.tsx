64133dbf014c0a78dedb4a172a2f7786fad54d16
Rod Cotines <rodcotines@gmail.com>
2023-08-14 16:05:55 +0800

    updated faq page

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -28,40 +28,40 @@ const faq = () => {
   ];
 
   return (
-    <div className="w-full min-h-screen h-auto py-4 relative px-5 sm:flex sm:items-center md:items-center md:justify-center ">
+    <div className="w-full min-h-screen h-auto relative sm:flex sm:items-center md:items-center md:justify-center flex flex-col ">
       <div className="max-w-[1400px] ">
         {/* FAQ HERO */}
         <div className="flex items-center justify-center">
-          <div className="w-[50%]">
-            <h1 className="lg:text-[50px] text-left text-nav flex-col font-bold font-primary">
+          <div className="lg:w-[50%] px-5">
+            <h1 className="text-3xl md:text-5xl lg:text-7xl text-center  text-nav flex-col font-bold font-primary p-8 ">
               FREQUENTLY ASKED QUESTIONS
             </h1>
-            <div className="font-inter">
-              <p className="text-black/70">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Sapiente dolores quisquam fugit perspiciatis quae vel? Quas,
-                modi ipsum. Magni distinctio nam maiores. Deserunt reiciendis,
-                omnis maxime fuga molestias ullam minima.
-              </p>
-            </div>
+            <p className="text-black/70 font-inter text-xl">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
+              dolores quisquam fugit perspiciatis quae vel? Quas, modi ipsum.
+              Magni distinctio nam maiores. Deserunt reiciendis, omnis maxime
+              fuga molestias ullam minima.
+            </p>
           </div>
           <div className="w-[50%] lg:block hidden">
             <Image
-              src="/hut.webp"
+              src="/faqlog.webp"
               alt="kubo"
               width="1000"
               height="1000"
-              className="w-[45rem] mt-10 border-black"
+              className="w-[45rem] mt-10 rounded-md"
             />
           </div>
         </div>
 
         {/* ACCORDIONS */}
-        <div className="w-full h-auto mt-32">
-          <h1 className="text-5xl text-nav text-center font-bold">Questions</h1>
+        <div className="w-full h-auto mt-32 px-5">
+          <h1 className="md:text-5xl lg:text-7xl text-3xl text-nav text-center font-bold">
+            Questions
+          </h1>
 
-          <div className="flex flex-row  mt-10  ">
-            <div className=" flex flex-col w-[50%] gap-5 p-2">
+          <div className="flex md:flex-row  mt-10 flex-col items-center justify-center  ">
+            <div className=" flex flex-col md:w-[50%] gap-5 p-2">
               {faqList.map((items, i) => {
                 return (
                   <Questions
@@ -72,7 +72,7 @@ const faq = () => {
                 );
               })}
             </div>
-            <div className=" flex flex-col w-[50%] gap-5 p-2 ">
+            <div className=" flex flex-col md:w-[50%] gap-5 p-2 md:al">
               {faqList.map((items, i) => {
                 return (
                   <Questions
@@ -85,21 +85,27 @@ const faq = () => {
             </div>
           </div>
         </div>
-        {/* CONTACT US */}
-        <div className="max-w-full md:w-full h-[356px]  flex flex-col items-center justify-center py-2 mt-28 ">
-          <h1 className="text-[#D1770E] md:text-[80px] font-bold md:text-center text-[48px] sm:text-[60px] font-primary">
-            {" "}
-            •GET IN TOUCH•
-          </h1>
-          <h1 className="text-black/50 md:text-[50px] font-bold md:text-center text-[30px] sm:text-[40px] font-secondary">
-            {" "}
-            Contact us @
-          </h1>
-          <h1 className="text-black/50 md:text-[45px] font-bold md:text-center text-[20px] sm:text-[40px] font-secondary ">
-            {" "}
-            digital.kubo@example.com
-          </h1>
-        </div>
+      </div>
+      {/* CONTACT US */}
+      <div
+        className="max-w-full w-full h-[500px] flex flex-col items-center justify-center py-2 mt-28 bg-cover "
+        style={{
+          backgroundImage: `url('/bg-faq.webp')`,
+          height: "500px",
+        }}
+      >
+        <h1 className="text-white md:text-[80px] font-bold md:text-center text-[30px] sm:text-[60px] font-primary">
+          {" "}
+          •GET IN TOUCH•
+        </h1>
+        <h1 className="text-white md:text-[50px] font-bold md:text-center text-[25px] sm:text-[40px] font-secondary">
+          {" "}
+          Contact us @
+        </h1>
+        <h1 className="text-white md:text-[45px] font-bold md:text-center text-[20px] sm:text-[40px] font-secondary ">
+          {" "}
+          digital.kubo@example.com
+        </h1>
       </div>
     </div>
   );
