5984377c9154eadd8087ca9a3944d0412342b1da
Rod Cotines <rodcotines@gmail.com>
2023-08-28 19:26:51 +0800

    auto redirect to home login/browse

--- src/pages/browse.tsx
+++ src/pages/browse.tsx
@@ -1,16 +1,41 @@
 import Hero from "@/views/browse/Hero";
 import AP from "@/views/browse/AP";
+import { GetServerSideProps } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/home",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+};
+
 const browse = () => {
-    return (
-        <>
-            <div style={{
-                backgroundImage: `url('bg.webp')`
-            }}>
-                <Hero/>
-                <AP/>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage: `url('bg.webp')`,
+        }}
+      >
+        <Hero />
+        <AP />
+      </div>
+    </>
+  );
+};
 
 export default browse;
