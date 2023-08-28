e065f17da81b731e0b088fec55243525c3d1fe42
Rod Cotines <rodcotines@gmail.com>
2023-08-28 18:48:08 +0800

    change the footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,15 @@
 const Footer = () => {
-    return (
-        <footer className="w-full p-5 h-auto bg-[#0a1620] text-white">
-            <p className="text-sm font-medium tracking-wide">Built by EyedOwl</p>
-        </footer>
-    );
-}
+  return (
+    <footer className="w-full p-5 h-auto  text-white flex items-center justify-between">
+      <p className="text-sm  tracking-widest text-black flex">
+        ALL RIGHTS RESERVED © 2023 DIGITALKUBO{" "}
+      </p>
+      <p className="text-black text-sm tracking-widest ">
+        {" "}
+        PRIVACY POLICY | TERMS AND CONDITIONS
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
